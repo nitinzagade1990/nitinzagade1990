03febc3c4433665441341705d63c8543096d55f0
nitinzagade1990 <nitin.zagade1990@gmail.com>
2021-09-12 13:57:06 +0530

    created the constants for the activity param and database

---
+++ app/src/main/java/com/example/proximitytest/constants/ActivityParamConstants.kt
@@ -0,0 +1,7 @@
+package com.example.proximitytest.constants
+
+class ActivityParamConstants {
+    companion object {
+        val CITY_NAME = "city_name"
+    }
+}
---
+++ app/src/main/java/com/example/proximitytest/constants/DbConstants.kt
@@ -0,0 +1,10 @@
+package com.example.proximitytest.constants
+
+class DbConstants {
+
+    companion object {
+        const val TABLE_NAME_CITY = "city_table"
+        const val TABLE_NAME_CITY_HISTORY = "city_history"
+    }
+
+}
