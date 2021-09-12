27e06ea65a57b0a87878d1718b1a4b4501bddb70
nitinzagade1990 <nitin.zagade1990@gmail.com>
2021-09-12 14:00:50 +0530

    dateconverter class added

--- app/src/main/java/com/example/proximitytest/database/converter/DateConverter.kt
+++ app/src/main/java/com/example/proximitytest/database/converter/DateConverter.kt
@@ -1,4 +1,18 @@
 package com.example.proximitytest.database.converter
 
+import androidx.room.TypeConverter
+import java.util.*
+
 class DateConverter {
+
+    @TypeConverter
+    fun toDate(dateLong: Long?): Date? {
+        return dateLong?.let { Date(it) }
+    }
+
+    @TypeConverter
+    fun fromDate(date: Date?): Long? {
+        return if (date == null) null else date.getTime()
+    }
+
 }
