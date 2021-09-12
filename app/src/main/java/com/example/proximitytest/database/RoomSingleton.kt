385f597dd343a1134d56e63cff40aeb7957b8a48
nitinzagade1990 <nitin.zagade1990@gmail.com>
2021-09-12 14:02:58 +0530

    created the singletome class to  for the oom database

--- app/src/main/java/com/example/proximitytest/database/RoomSingleton.kt
+++ app/src/main/java/com/example/proximitytest/database/RoomSingleton.kt
@@ -1,4 +1,28 @@
 package com.example.proximitytest.database
 
-class RoomSingleton {
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import android.content.Context
+import com.example.proximitytest.database.citydata.City
+import com.example.proximitytest.database.citydata.CityHistory
+import com.example.proximitytest.database.dao.CityDao
+import com.example.proximitytest.database.dao.CityHistoryDao
+
+
+@Database(entities = [City::class, CityHistory::class], version = 1)
+abstract class RoomSingleton : RoomDatabase() {
+    abstract fun getCityDao(): CityDao
+    abstract fun getCityHistoryDao(): CityHistoryDao
+
+    companion object {
+        private var INSTANCE: RoomSingleton? = null
+        fun getInstance(context: Context): RoomSingleton {
+            if (INSTANCE == null) {
+                INSTANCE = Room.databaseBuilder(context, RoomSingleton::class.java, "roomdb")
+                    .allowMainThreadQueries().build()
+            }
+            return INSTANCE as RoomSingleton
+        }
+    }
 }
