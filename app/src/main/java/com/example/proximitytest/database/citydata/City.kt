b61b059ac9bf2e0e5a35bdd909de2748aaf70697
nitinzagade1990 <nitin.zagade1990@gmail.com>
2021-09-12 13:59:40 +0530

    model classed for storing the data against the city history and city

--- app/src/main/java/com/example/proximitytest/database/citydata/City.kt
+++ app/src/main/java/com/example/proximitytest/database/citydata/City.kt
@@ -1,16 +1,17 @@
-package com.example.proximitytest.database
+package com.example.proximitytest.database.citydata
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import androidx.room.TypeConverters
+import com.example.proximitytest.constants.DbConstants
 import com.example.proximitytest.database.converter.DateConverter
 import com.google.gson.annotations.SerializedName
 import java.time.Instant
 import java.time.OffsetDateTime
 import java.util.*
 
-@Entity(tableName = "city_table")
+@Entity(tableName = DbConstants.TABLE_NAME_CITY)
 @TypeConverters(DateConverter::class)
 data class City(
     @PrimaryKey val city:
@@ -22,3 +23,5 @@ data class City(
 
 
 )
+
+
--- app/src/main/java/com/example/proximitytest/database/citydata/CityHistory.kt
+++ app/src/main/java/com/example/proximitytest/database/citydata/CityHistory.kt
@@ -1,24 +1,21 @@
-package com.example.proximitytest.database
+package com.example.proximitytest.database.citydata
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-import androidx.room.TypeConverters
+import androidx.room.*
+import com.example.proximitytest.constants.DbConstants
 import com.example.proximitytest.database.converter.DateConverter
 import com.google.gson.annotations.SerializedName
 import java.time.Instant
 import java.time.OffsetDateTime
 import java.util.*
 
-@Entity(tableName = "city_table")
-@TypeConverters(DateConverter::class)
-data class City(
-    @PrimaryKey val city:
-    String,
+@Entity(tableName = DbConstants.TABLE_NAME_CITY_HISTORY)
+
+data class CityHistory(
+    val city: String,
     val aqi: Float,
-
     var lastModified: Long,
-    var textColo: Int
 
-
-)
+    ) {
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0
+}
